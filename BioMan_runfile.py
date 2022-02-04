f3b300368d9ea2e1399bd0fed648899c79817c4a
yayati-katkade <85357666+yayati-katkade@users.noreply.github.com>
2022-02-04 11:46:45 -0700

    Update BioMan_runfile.py

--- BioMan_runfile.py
+++ BioMan_runfile.py
@@ -7,7 +7,7 @@ It's discrete params of Bioman.
 
 import pandas as pd
 import numpy as np
-import pyDOE2
+#import pyDOE2
 import random
 import matplotlib.pyplot as plt
 import timeit
@@ -43,19 +43,58 @@ def alpha_values(i,j):
     return alpha_low_ll, alpha_low_ul, alpha_up_ll, alpha_up_ul, alpha_low_mfg_fact, alpha_up_mfg_fact, low_level_factor_mfg, up_level_factor_mfg
 
 #We have 2 values of delta either low or high
-def delta_value(i):
+def Delta_mix(i):
     if i == 0: 
-        delta_low=2 
-        delta_up =6
-        delta_fact='low'
+        delta_mix_1 = 0.5 
+        delta_mix_2 = 0.25
 
-    else: 
-        delta_low=6
-        delta_up =10
-        delta_fact='high'
+    elif i ==1:
+        delta_mix_1 = 0.25
+        delta_mix_2 = 0.25
 
-    return delta_low, delta_up, delta_fact
+    elif i ==2:
+        delta_mix_1 = 0.25
+        delta_mix_2 = 0.5 
+
+    elif i ==3:    
+        delta_mix_1 = 0.33
+        delta_mix_2 = 0.33
+
+    #else: 
+    #    delta_low=6
+    #    delta_up =10
+    #    delta_fact='high'
+
+    return delta_mix_1, delta_mix_2 
     
+
+
+#Manufacturing_Mix(Manufacturing_Mix_MFG)
+def Manufacturing_Mix(Manufacturing_Mix_MFG):
+    if Manufacturing_Mix_MFG == 0:
+        mfg_time_separator_1 = 0.5
+        mfg_time_separator_2 = 0.25
+
+
+    elif Manufacturing_Mix_MFG == 1:
+        mfg_time_separator_1 = 0.25
+        mfg_time_separator_2 = 0.25
+
+
+    elif Manufacturing_Mix_MFG == 2:
+        mfg_time_separator_1 = 0.25
+        mfg_time_separator_2 = 0.5
+
+
+
+    elif Manufacturing_Mix_MFG == 3:
+        mfg_time_separator_1 = 0.33
+        mfg_time_separator_2 = 0.33
+
+
+    return mfg_time_separator_1, mfg_time_separator_2
+    # mfg_time_separator_1, mfg_time_separator_2 = Manufacturing_Mix(Manufacturing_Mix_MFG)
+
 
 def Patient_Mix(Patient_Mix_MFG):
     if Patient_Mix_MFG == 0:
@@ -64,25 +103,31 @@ def Patient_Mix(Patient_Mix_MFG):
         good_pat_separator = 0.1 
 		#100% relaxed system
 
-    elif Patient_Mix_MFG == 1:
+    """
+    #elif Patient_Mix_MFG == 1:
         bad_pat_separator = 0.5
         average_pat_separator = 0.3
         good_pat_separator = 0.2
         #0% stressed system
+    """
 
-    elif Patient_Mix_MFG == 2:
+    #elif Patient_Mix_MFG == 2:
+    if Patient_Mix_MFG == 1:
         bad_pat_separator = 0.1
         average_pat_separator = 0.8
         good_pat_separator = 0.1
     	#50% relaxed system
-
+    
+    """
     elif Patient_Mix_MFG == 3:
         bad_pat_separator = 0.2
         average_pat_separator = 0.5
         good_pat_separator = 0.3
     	#25% relaxed system 75% stressed system
+    """
 
-    elif Patient_Mix_MFG == 4:
+    #elif Patient_Mix_MFG == 4:
+    if Patient_Mix_MFG == 2:
         bad_pat_separator = 0.1
         average_pat_separator = 0.1
         good_pat_separator = 0.8
@@ -98,25 +143,31 @@ def System_Mix(System_Mix_MFG):
         relaxed_sys_separator = 0.1 
         #100% relaxed system
 
+    """
     elif System_Mix_MFG == 1:
         stressed_sys_separator = 0.5
         average_sys_separator = 0.3
         relaxed_sys_separator = 0.2
         #0% stressed system
+    """
 
-    elif System_Mix_MFG == 2:
+    #elif System_Mix_MFG == 2:
+    if System_Mix_MFG == 1:
         stressed_sys_separator = 0.1
         average_sys_separator = 0.8
         relaxed_sys_separator = 0.1
         #50% relaxed system
 
+    """
     elif System_Mix_MFG == 3:
         stressed_sys_separator = 0.2
         average_sys_separator = 0.5
         relaxed_sys_separator = 0.3
         #25% relaxed system 75% stressed system
+    """
 
-    elif System_Mix_MFG == 4:
+
+    if System_Mix_MFG == 2:
         stressed_sys_separator = 0.1
         average_sys_separator = 0.1
         relaxed_sys_separator = 0.8
@@ -292,29 +343,33 @@ def Main():
     #Hrv Operator and Machines FIX ; 2 levels of machines and 2 of operators (ratio of machine)
     #levels = [alphalow, alphaup, delta, Patient mix, System mix, QM Policy, Hrv Operator, Hrv Machines, Mfg Operators, Mfg Machines]
     #levels = [2,2,2,5,5,1,2,2,2,2]    # this is the levels we are following
-    levels =  [2,2,2,5,5,1,2,2,2,2]
+
+    #levels = [Manufacturing Time mix, Delta mix, Patient mix, System mix, QM Policy, Hrv Operator, Hrv Machines, Mfg Operators, Mfg Machines]
+    levels =  [4,4,5,5,1,2,2,2,2]
 
 
-
-    total_design = pyDOE2.fullfact(levels)
-    print(len(total_design))
+    start_num=1
+    #total_design = pyDOE2.fullfact(levels)
+    #print(len(total_design))
     #print(total_design)        #give the entire list of all possible scenarios of the levels
     
 
-    print(random.choice(total_design))
+    #print(random.choice(total_design))
     print('START SIM')
     design_run = 0
-    print(type(total_design))
+    #print(type(total_design))
 
 
-
+    #main_df = pd.DataFrame(columns=['Level Number','Iteration Number','Clock', 'Event name', 'Event ID', 'Event happen time', 'Event place', 'Event machine', 'Event operator', 'Event job', 'Event rework times', 'Job yield', 
+     #                                               'hrv_operator_state_list', 'MFG_operator_state_list', 'QC_operator_state_list', 'hrv_machine_state_list', 'MFG_machine_state_list', 'queue_state_list', 'job_state_list',
+                                                    
+      
 
 
     main_df=pd.DataFrame(columns=['Level Number','Iteration Number','Clock', 'Event name', 'Event ID', 'Event happen time', 'Event place', 'Event machine', 'Event operator', 'Event job', 'Event rework times', 'Job yield', 
                                                     'hrv_operator_state_list', 'MFG_operator_state_list', 'QC_operator_state_list', 'hrv_machine_state_list', 'MFG_machine_state_list', 'queue_state_list', 'job_state_list',
-                                                    'jobs_in_queue', 'jobs_in_rework', 'jobs_in_service','jobs_in_qc', 'jobs_in_completed', 'total_job_num'])
-
-
+                                                    'jobs_entered', 'job_booked','jobs_in_queue', 'jobs_in_q1','jobs_in_q2','jobs_in_q3','jobs_in_rework', 'jobs_in_service','jobs_in_qc', 'jobs_in_completed', 'total_job_num'])
+                                                    
         #self.df_this_design = pd.DataFrame(columns=['Clock', 'Event name', 'Event ID', 'Event happen time', 'Event place', 'Event machine', 'Event operator', 'Event job', 'Event rework times', 'Job yield', 
         #                                            'hrv_operator_state_list', 'MFG_operator_state_list', 'QC_operator_state_list', 'hrv_machine_state_list', 'MFG_machine_state_list', 'queue_state_list', 'job_state_list',
          #                                           'jobs_in_queue', 'jobs_in_rework', 'jobs_in_service','jobs_in_qc', 'jobs_in_completed', 'total_job_num'])
@@ -323,6 +378,15 @@ def Main():
     df_mainjob=pd.DataFrame(columns=['Level Number','Iteration Number','Job_number','Alpha_low_mfg','Alpha_up_mfg','Delta_mfg','Gender','Blood vol','Patient Trgt Bld Count','Start_Time','Processing_Time','Rework Time','End Time'])    
         
 
+    #expt_levels=open("Expt_level1.txt", "r")
+    #for x in expt_level:
+       # print(x)
+
+    bioman_exp_table_fname="Bio_man_exp_table.csv"   #agave path
+    EXP_COUNT=1
+    
+    experiments = pd.read_csv(bioman_exp_table_fname)
+    experiments=experiments.replace('^L','',regex=True).astype(int)-1
 
 
 
@@ -331,8 +395,7 @@ def Main():
 
 
 
-
-
+    """
     #generate random value list
     rand_level_list = []
     i=0
@@ -342,41 +405,93 @@ def Main():
 
     with open('Experiment Level.txt', 'w') as f:
         for item in rand_level_list:
-            f.write("%s\n" % item)    
+            f.write("%s" % item)    
 
     for iteration_num in range(0,5):
         print(iteration_num)
 
     #print(rand_level_list)
+    """
+
+
 
     design_run =0
+    
+
     #for design in total_design:
-    for  design in rand_level_list:
+    #for  design in rand_level_list:
+    start_exp = (start_num-1)*10 
+    
+    #for x in range(start_exp, EXP_COUNT+start_exp):
+    #for x in range(0, 20):
+    for x in range(0, 1):
+
+        
+        row=experiments[x:x+1]     
+
+
+
         design_run += 1
         iteration = 0
 
         main_df=pd.DataFrame(columns=['Level Number','Iteration Number','Clock', 'Event name', 'Event ID', 'Event happen time', 'Event place', 'Event machine', 'Event operator', 'Event job', 'Event rework times', 'Job yield', 
                                                     'hrv_operator_state_list', 'MFG_operator_state_list', 'QC_operator_state_list', 'hrv_machine_state_list', 'MFG_machine_state_list', 'queue_state_list', 'job_state_list',
-                                                    'jobs_in_queue', 'jobs_in_rework', 'jobs_in_service','jobs_in_qc', 'jobs_in_completed', 'total_job_num'])
-
+                                                    'jobs_entered', 'job_booked','jobs_in_queue', 'jobs_in_q1','jobs_in_q2','jobs_in_q3','jobs_in_rework', 'jobs_in_service','jobs_in_qc', 'jobs_in_completed', 'total_job_num'])
+    
 
             #self.df_this_design = pd.DataFrame(columns=['Clock', 'Event name', 'Event ID', 'Event happen time', 'Event place', 'Event machine', 'Event operator', 'Event job', 'Event rework times', 'Job yield', 
             #                                            'hrv_operator_state_list', 'MFG_operator_state_list', 'QC_operator_state_list', 'hrv_machine_state_list', 'MFG_machine_state_list', 'queue_state_list', 'job_state_list',
              #                                           'jobs_in_queue', 'jobs_in_rework', 'jobs_in_service','jobs_in_qc', 'jobs_in_completed', 'total_job_num'])
         
             #df to collect job datas
-        df_mainjob=pd.DataFrame(columns=['Level Number','Iteration Number','Job_number','Alpha_low_mfg','Alpha_up_mfg','Delta_mfg','Gender','Blood vol','Patient Trgt Bld Count','Start_Time','Processing_Time','Rework Time','End Time'])    
+        df_mainjob=pd.DataFrame(columns=['Level Number','Iteration Number','Job_number','Alpha_low_mfg','Alpha_up_mfg','Delta_mfg','Manufacturing_Mix','Gender','Blood vol','Patient Trgt Bld Count','Start_Time','Processing_Time','Rework Time','End Time'])    
+
 
         #print("Level")
         #print(design_run)
         #print("Iteration")
+        """
+        alphalow=row["alpha_low"].values[0]
+        alphaup= row["alpha_high"].values[0]
+        delta = row["delta"].values[0]
+        Patient_Mix_MFG = row["patient_mix"].values[0]
+        System_Mix_MFG = row["system_mix"].values[0]
+        QM_Policy_MFG = 0
+        Hrv_operator=row["Hrv_operator"].values[0]
+        Hrv_machine=row["Hrv_machine"].values[0]
+        Mfg_operator=row["Mfg_operator"].values[0]
+        Mfg_machine=row["Mfg_machine"].values[0]
+        """
+        
+        Manufacturing_Mix_MFG =row["Mfg Time"].values[0]   #change in excel
+        Delta_Mix  = row["Delta Mix"].values[0]
+
+#        Patient_Mix_MFG, System_Mix_MFG, QM_Policy_MFG,Hrv_machine,Hrv_operator,Mfg_machine,Mfg_operator
+        
+
+        Patient_Mix_MFG = row["patient_mix"].values[0]
+        #System_Mix_MFG = row["system_mix"].values[0]
+        QM_Policy_MFG = 0
+        Hrv_operator=row["Hrv_operator"].values[0]
+        Hrv_machine=row["Hrv_machine"].values[0]
+        Mfg_operator=row["Mfg_operator"].values[0]
+        Mfg_machine=row["Mfg_machine"].values[0]
+
+        
+        #print("HElloOsdasjkfsdkjsad")
+        #print(alphalow,alphaup, delta,Patient_Mix_MFG, System_Mix_MFG, QM_Policy_MFG)
+        #alpha_low_ll, alpha_low_ul, alpha_up_ll, alpha_up_ul, alpha_low_mfg_fact, alpha_up_mfg_fact, low_level_factor_mfg, up_level_factor_mfg=alpha_values(alphalow,alphaup)
+        
+
+        delta_mix_1, delta_mix_2=Delta_mix(Delta_Mix) 
+
+        design= [Manufacturing_Mix_MFG, Delta_Mix, Patient_Mix_MFG, QM_Policy_MFG, Hrv_machine, Hrv_operator, Mfg_machine, Mfg_operator]
 
 
 
+        ITERATION = 1  #replication number
 
-
-
-        for iteration_num in range(0,1):
+        for iteration_num in range(0,ITERATION):
             
 
             #print(iteration_num)
@@ -384,6 +499,7 @@ def Main():
 
             design=list(design)
             #print('design',design)
+            #design = [1,1,1,0,0,0,1,1,1,1]
             print(design)
             print('***Start***')
             print('This design:', design)
@@ -404,26 +520,38 @@ def Main():
             print('____________________________________')
             """
             
-            alphalow=design[0]
-            alphaup= design[1]
-            delta = design[2]
-            Patient_Mix_MFG = design[3]
-            System_Mix_MFG = design[4]
-            QM_Policy_MFG = design[5]
+            #alphalow=design[0]
+            #alphaup= design[1]
+            
+            #Manufacturing_Mix_MFG
+
+            #design= [Manufacturing_Mix_MFG,Delta_Mix, Patient_Mix_MFG, System_Mix_MFG, QM_Policy_MFG,Hrv_machine,Hrv_operator,Mfg_machine,Mfg_operator]
+
+            Manufacturing_Mix_MFG=design[0]
+
+            delta = design[1]
+            Patient_Mix_MFG = design[2]
+            #System_Mix_MFG = design[3]
+            QM_Policy_MFG = design[3]
             
             
             #Hrv Operator = design[6], Hrv Machines, Mfg Operators, Mfg Machines
             
-            alpha_low_ll, alpha_low_ul, alpha_up_ll, alpha_up_ul, alpha_low_mfg_fact, alpha_up_mfg_fact, low_level_factor_mfg, up_level_factor_mfg=alpha_values(alphalow,alphaup)
-            delta_ll, delta_ul, delta_fact=delta_value(delta)  
+            #alpha_low_ll, alpha_low_ul, alpha_up_ll, alpha_up_ul, alpha_low_mfg_fact, alpha_up_mfg_fact, low_level_factor_mfg, up_level_factor_mfg=alpha_values(alphalow,alphaup)
+            #delta_ll, delta_ul, delta_fact=delta_value(delta)  
             #separator_1, separator_2 = Patient_Mix(Patient_Mix_MFG)
             
 
             bad_pat_separator, average_pat_separator, good_pat_separator= Patient_Mix(Patient_Mix_MFG) #prob_stress
-            stressed_sys_separator, average_sys_separator, relaxed_sys_separator=System_Mix(System_Mix_MFG)
+            #stressed_sys_separator, average_sys_separator, relaxed_sys_separator=System_Mix(System_Mix_MFG)            
+            delta_mix_1, delta_mix_2 = Delta_mix(delta)
+            mfg_time_separator_1, mfg_time_separator_2 = Manufacturing_Mix(Manufacturing_Mix_MFG)
+
+            low_level_factor_mfg = 0.9 
+            up_level_factor_mfg = 1.1
             #OurController = Factory.Controller()        
             OurEnvironment = Factory.Environment(design)
-            OurEnvironment.Factor_design(alpha_low_ll, alpha_low_ul,alpha_up_ll, alpha_up_ul, delta_ll, delta_ul, low_level_factor_mfg, up_level_factor_mfg, bad_pat_separator, average_pat_separator, good_pat_separator, stressed_sys_separator, average_sys_separator, relaxed_sys_separator, QM_Policy_MFG)
+            OurEnvironment.Factor_design(mfg_time_separator_1, mfg_time_separator_2, delta_mix_1, delta_mix_2, low_level_factor_mfg, up_level_factor_mfg, bad_pat_separator, average_pat_separator, good_pat_separator, QM_Policy_MFG)
             start = timeit.default_timer()
             this_df, df_job = OurEnvironment.Simulate()
             print("SIM DONE")
@@ -468,7 +596,7 @@ def Main():
             this_df['qc_opr_busy_cnt'] = this_df.apply(lambda row: row['QC_operator_state_list'].count("busy") , axis=1)
             this_df['qc_opr_booked_cnt'] = this_df.apply(lambda row: row['QC_operator_state_list'].count("booked") , axis=1)        
             
-     		
+            
 
             #add a new df to get all job data
             
@@ -492,8 +620,8 @@ def Main():
 
 
             #converting df to csv files 
-            this_df.to_csv('{}experiment_output_design{}{}.csv'.format(design_run,design_run,iteration_num))
-            df_job.to_csv('{}job_data{}{}.csv'.format(design_run,design_run,iteration_num))
+            this_df.to_csv('{}experiment_output_design{}_{}.csv'.format(design_run,design_run,iteration_num))
+            df_job.to_csv('{}job_data{}_{}.csv'.format(design_run,design_run,iteration_num))
 
             
 
@@ -521,6 +649,10 @@ def Main():
             plot_cell_state_job_abs(this_df,design_run,iteration_num)
             #plot_cell_state_graph_for_abs_value(this_df,design_run,'mfg_opr_busy_cnt')
             
+            plot_cell_state_job(this_df,design_run,iteration_num)
+
+
+            
             plot_cell_state_graph_for_abs_value(this_df,design_run,iteration_num,'mfg_opr_idle_cnt')
             plot_cell_state_graph_for_abs_value(this_df,design_run,iteration_num,'hrv_mc_busy_cnt')
             plot_cell_state_graph_for_abs_value(this_df,design_run,iteration_num,'hrv_mc_idle_cnt')
@@ -546,7 +678,7 @@ def Main():
             #print('Time: ', stop - start)
             
         
-            if iteration_num == 0:
+            if iteration_num == ITERATION:
 
 
             #this_df['run_number'] = design_run
@@ -557,17 +689,29 @@ def Main():
                 break
 
                 #just for test--------
-        if design_run == 10:
-        	#stop = timeit.default_timer()
-        	#print('Time: ', stop - start)
-        	break
+        if design_run == 81:
+            #stop = timeit.default_timer()
+            #print('Time: ', stop - start)
+            break
 
         #---------------------
         
         #return
-	
-Main()
+#**********************************
+"""
+import argparse
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Bio_man Simulation')
+    parser.add_argument('start_num', type=int, nargs=1, help='Bio_man Simulation')
+    args = parser.parse_args()
 
+    start_num = args.start_num[0]
+    
+print('start_num:', start_num)
+Main(start_num)
+"""
+Main()
+#command: python BioMan_runfile.py start_num
 
 """
 # main function
